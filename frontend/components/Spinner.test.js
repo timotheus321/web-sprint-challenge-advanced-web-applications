98123701eb5a41b617c60dde9393627d9f15d4c3
Timothy White <bigtball321@yahoo.com>
2023-07-13 13:59:29 -0400

    finished

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,6 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import Spinner from './Spinner'
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
